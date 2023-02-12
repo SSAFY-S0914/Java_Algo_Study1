4f158ec61f7a1b8ff6e3517ea8e68da42f455164
synodical <lena0323park@naver.com>
2023-02-12 19:55:08 +0900

    [4주차]이예은 백준 9663, 백준 13164 문제풀이

---
+++ study/week4/JUN13164_yeeun.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class JUN13164_yeeun {
+    static int n, k;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1];
+        List<Integer> sub = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < n-1; i++) {
+            sub.add(arr[i + 1] - arr[i]);
+            ans += sub.get(i);
+        }
+        Collections.sort(sub, Collections.reverseOrder());
+        for (int i = 0; i < k-1; i++) {
+            ans -= sub.get(i);
+        }
+        System.out.println(ans);
+    }
+}
